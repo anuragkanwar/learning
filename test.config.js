104ce9535e4f84cd0a81eb5fb9ae9c35e759832a
anurag kanwar <aigkaraig@gmail.com>
2025-02-14 12:22:23 +0530

    pr workflow check 7

--- test.config.js
+++ test.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://anuragkanwar.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'withthis',
+  baseUrl: '/whatever_here/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
